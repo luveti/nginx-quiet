23c65ce3f7717a5384a6be4f57f6fe3c0aa9b459
luveti <tu4756@gmail.com>
2020-10-31 01:22:33 -0400

    Handle having only a start index or an end index - otherwise tracebacks don't get the request info removed

--- nginx-quiet.lua
+++ nginx-quiet.lua
@@ -10,17 +10,18 @@ local function process_line(line)
 	-- find the start of what I believe is the connection id
 	-- SEE https://git.io/JT9oC
 	local _, start_idx = line:find(': *', 1, true)
-	if not start_idx then return log(line) end
-
-	-- find the space after the connection id
-	_, start_idx = line:find(' ', start_idx, true)
-	if not start_idx then return log(line) end
+	if start_idx then
+		-- find the space after the connection id
+		_, start_idx = line:find(' ', start_idx, true)
+	end
+	if not start_idx then start_idx = 0 end
+	start_idx = start_idx + 1
 
 	-- find the end, this looks for the start of the request info
 	local end_idx = line:find(', client: ', start_idx, true)
-	if not end_idx then return log(line:sub(start_idx + 1)) end
+	if end_idx then end_idx = end_idx - 1 end
 
-	log(line:sub(start_idx + 1, end_idx - 1))
+	log(line:sub(start_idx, end_idx))
 end
 
 while true do
